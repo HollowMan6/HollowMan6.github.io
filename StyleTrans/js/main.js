1f5df88d1427a526b31d8c49d62ccfd5809f004e
hollowman6 <hollowman186@vip.qq.com>
2019-10-02 19:02:44 +0800

    Latest push

---
+++ StyleTrans/js/main.js
@@ -0,0 +1,206 @@
+jQuery(document).ready(function($) {
+
+    /* ---------------------------------------------------------------------- */
+    /*	------------------------------- Loading ----------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    /*Page Preloading*/
+    $(window).load(function() {
+        $('#spinner').fadeOut(200);
+        $('#preloader').delay(200).fadeOut('slow');
+        $('.wrapper').fadeIn(200);
+        $('#custumize-style').fadeIn(200);
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------------- Taps profile ------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $('.collapse_tabs').click(function() {
+
+        if ($(this).hasClass('collapsed')) {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        } else {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+        }
+
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------- easyResponsiveTabs ------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $('#verticalTab').easyResponsiveTabs({
+        type: 'vertical',
+        width: 'auto',
+        fit: true
+    });
+
+    $("h2.resp-accordion").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $("h2.resp-accordion").not(this).find(".icon_menu").removeClass("icon_menu_active");
+
+        /*	Scroll To */
+        $('html, body').animate({scrollTop: $('h2.resp-accordion').offset().top - 50}, 600);
+    });
+
+    $(".resp-tabs-list li").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $(".resp-tabs-list li").not(this).find(".icon_menu").removeClass("icon_menu_active");
+    });
+
+
+    $(".resp-tabs-list li").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    $("h2.resp-accordion").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* --------------------------- Scroll tabs ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $(".content_2").mCustomScrollbar({
+        theme: "dark-2",
+        contentTouchScroll: true,
+        advanced: {
+            updateOnContentResize: true,
+            updateOnBrowserResize: true,
+            autoScrollOnFocus: false
+        }
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------- Effect tabs -------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    var animation_style = 'bounceIn';
+
+    $('.dropdown-select').change(function() {
+        animation_style = $('.dropdown-select').val();
+    });
+
+
+    $('ul.resp-tabs-list li[class^=tabs-]').click(function() {
+
+        var tab_name = $(this).attr('data-tab-name');
+
+        $('.resp-tabs-container').addClass('animated ' + animation_style);
+        $('.resp-tabs-container').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('.resp-tabs-container').removeClass('animated ' + animation_style);
+        });
+
+        $(".content_2").mCustomScrollbar("destroy");
+        $(".content_2").mCustomScrollbar({
+            theme: "dark-2",
+            contentTouchScroll: true,
+            advanced: {
+                updateOnContentResize: true,
+                updateOnBrowserResize: true,
+                autoScrollOnFocus: false
+            }
+        });
+
+        return false;
+    });
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------- redimensionnement ----------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    function redimensionnement() {
+
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".resp-vtabs .resp-tabs-container").css("height", "100%");
+            $(".content_2").css("height", "100%");
+        } else {
+
+            $(".resp-vtabs .resp-tabs-container").css("height", "580px");
+            $(".content_2").css("height", "580px");
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".content_2").mCustomScrollbar({
+                theme: "dark-2",
+                contentTouchScroll: true,
+                advanced: {
+                    updateOnContentResize: true,
+                    updateOnBrowserResize: true,
+                    autoScrollOnFocus: false
+                }
+            });
+
+        }
+
+    }
+
+    window.addEventListener('load', redimensionnement, false);
+    window.addEventListener('resize', redimensionnement, false);
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- Portfolio ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                // call the hover effect
+                onMixEnd: filterList.hoverEffect()
+            });
+
+        },
+        hoverEffect: function() {
+
+            // Simple parallax effect
+            $('#portfoliolist .portfolio').hover(
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: 0}, 200);
+                        $(this).find('img').stop().animate({top: -30}, 500);
+                    },
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: -40}, 200);
+                        $(this).find('img').stop().animate({top: 0}, 300);
+                    }
+            );
+
+        }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- prettyPhoto ---------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $("a[rel^='portfolio']").prettyPhoto({
+        animation_speed: 'fast', /* fast/slow/normal */
+        social_tools: '<div class="twitter"><a href="javascript:js_method();">选定此风格图片</a></div>',
+        theme: 'pp_default',
+        horizontal_padding: 5,
+        deeplinking: false,
+    });
+
+});
