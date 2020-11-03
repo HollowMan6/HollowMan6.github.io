080929e97bf6596754bfa23d727a4305401c3ff4
Hollow Man <hollowman@hollowman.ml>
2020-11-03 22:25:09 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

---
+++ js/nestcanvas.js
@@ -0,0 +1,135 @@
+class Circle {
+    //创建对象
+    //以一个圆为对象
+    //设置随机的 x，y坐标，r半径，_mx，_my移动的距离
+    //this.r是创建圆的半径，参数越大半径越大
+    //this._mx,this._my是移动的距离，参数越大移动
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.r = Math.random() * 10 ;
+        this._mx = Math.random() ;
+        this._my = Math.random() ;
+
+    }
+
+    //canvas 画圆和画直线
+    //画圆就是正常的用canvas画一个圆
+    //画直线是两个圆连线，为了避免直线过多，给圆圈距离设置了一个值，距离很远的圆圈，就不做连线处理
+    drawCircle(ctx) {
+        ctx.beginPath();
+        //arc() 方法使用一个中心点和半径，为一个画布的当前子路径添加一条弧。
+        ctx.arc(this.x, this.y, this.r, 0, 360)
+        ctx.closePath();
+        ctx.fillStyle = 'rgba(160, 160, 160, 0.2)';
+        ctx.fill();
+    }
+
+    drawLine(ctx, _circle) {
+        let dx = this.x - _circle.x;
+        let dy = this.y - _circle.y;
+        let d = Math.sqrt(dx * dx + dy * dy)
+        if (d < 150) {
+            ctx.beginPath();
+            //开始一条路径，移动到位置 this.x,this.y。创建到达位置 _circle.x,_circle.y 的一条线：
+            ctx.moveTo(this.x, this.y);   //起始点
+            ctx.lineTo(_circle.x, _circle.y);   //终点
+            ctx.closePath();
+            ctx.strokeStyle = 'rgba(160, 160, 160, 0.2)';
+            ctx.stroke();
+        }
+    }
+
+    // 圆圈移动
+    // 圆圈移动的距离必须在屏幕范围内
+    move(w, h) {
+        this._mx = (this.x < w && this.x > 0) ? this._mx : (-this._mx);
+        this._my = (this.y < h && this.y > 0) ? this._my : (-this._my);
+        this.x += this._mx / 2;
+        this.y += this._my / 2;
+    }
+}
+//鼠标点画圆闪烁变动
+class currentCirle extends Circle {
+    constructor(x, y) {
+        super(x, y)
+    }
+
+    drawCircle(ctx) {
+        ctx.beginPath();
+        //注释内容为鼠标焦点的地方圆圈半径变化
+        //this.r = (this.r < 14 && this.r > 1) ? this.r + (Math.random() * 2 - 1) : 2;
+        this.r = 8;
+        ctx.arc(this.x, this.y, this.r, 0, 360);
+        ctx.closePath();
+        //ctx.fillStyle = 'rgba(0,0,0,' + (parseInt(Math.random() * 100) / 100) + ')'
+        ctx.fillStyle = 'rgba(255, 77, 54, 0.6)'
+        ctx.fill();
+
+    }
+}
+
+! function() {
+    //封装方法，压缩之后减少文件大小
+    function get_by_tagname(name) {
+      return document.getElementsByTagName(name);
+    }
+    //设置canvas的高宽
+    function set_canvas_size() {
+      w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, 
+      h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    }
+
+    //更新页面用requestAnimationFrame替代setTimeout
+    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+    let draw = function () {
+        ctx.clearRect(0, 0, w, h);
+        for (let i = 0; i < circles.length; i++) {
+            circles[i].move(w, h);
+            circles[i].drawCircle(ctx);
+            for (j = i + 1; j < circles.length; j++) {
+                circles[i].drawLine(ctx, circles[j])
+            }
+        }
+        if (current_circle.x) {
+            current_circle.drawCircle(ctx);
+            for (var k = 1; k < circles.length; k++) {
+                current_circle.drawLine(ctx, circles[k])
+            }
+        }
+        requestAnimationFrame(draw)
+    }
+    
+    let init = function (num) {
+        for (var i = 0; i < num; i++) {
+            circles.push(new Circle(Math.random() * w, Math.random() * h));
+        }
+        draw();
+    }
+  
+    //创建画布，并添加到body中
+    var the_canvas = document.createElement("canvas"); //画布
+    let ctx = the_canvas.getContext('2d');
+    let w = the_canvas.width = the_canvas.offsetWidth;
+    let h = the_canvas.height = the_canvas.offsetHeight;
+    let circles = [];
+    let current_circle = new currentCirle(0, 0);
+    the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:-1";
+    get_by_tagname("body")[0].appendChild(the_canvas);
+  
+    //初始化画布大小
+    set_canvas_size();
+    window.onresize = set_canvas_size;
+    //当时鼠标位置存储，离开的时候，释放当前位置信息
+    window.onmousemove = function(e) {
+      e = e || window.event;
+      current_circle.x = e.clientX;
+      current_circle.y = e.clientY;
+    }, window.onmouseout = function() {
+      current_circle.x = null;
+      current_circle.y = null;
+    };
+    //0.1秒后绘制
+    window.addEventListener('load', init(60));
+  }();
---
+++ js/ribboncanvas.js
@@ -0,0 +1,59 @@
+! function () {
+    //设置canvas的高宽
+    function set_canvas_size() {
+        w = c.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+            h = c.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    }
+
+    document.addEventListener('touchmove', function (e) {
+        e.preventDefault()
+    })
+
+    //创建画布，并添加到body中
+    var c = document.createElement("canvas"); //画布
+    let x = c.getContext('2d');
+    let w = c.width = c.offsetWidth;
+    let h = c.height = c.offsetHeight;
+    c.style.cssText = "position:fixed;top:0;left:0;z-index:-1;opacity:0.6;";
+    document.getElementsByTagName("body")[0].appendChild(c);
+
+    //初始化画布大小
+    set_canvas_size();
+    window.onresize = set_canvas_size;
+    var f = 90,
+        q,
+        r = 0,
+        u = Math.PI * 2,
+        v = Math.cos,
+        z = Math.random;
+
+    function i() {
+        x.clearRect(0, 0, w, h)
+        q = [{ x: 0, y: h * .7 + f }, { x: 0, y: h * .7 - f }]
+        while (q[1].x < w + f) d(q[0], q[1])
+    }
+
+    function d(i, j) {
+        x.beginPath()
+        x.moveTo(i.x, i.y)
+        x.lineTo(j.x, j.y)
+        var k = j.x + (z() * 2 - 0.25) * f,
+            n = y(j.y)
+        x.lineTo(k, n)
+        x.closePath()
+        r -= u / -50
+        x.fillStyle = '#' + (v(r) * 127 + 128 << 16 | v(r + u / 3) * 127 + 128 << 8 | v(r + u / 3 * 2) * 127 + 128).toString(16)
+        x.fill()
+        q[0] = q[1]
+        q[1] = { x: k, y: n }
+    }
+
+    function y(p) {
+        var t = p + (z() * 2 - 1.1) * f
+        return (t > h || t < 0) ? y(p) : t
+    }
+
+    document.onclick = i
+    document.ontouchstart = i
+    i()
+}();
