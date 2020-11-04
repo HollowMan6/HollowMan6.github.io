48dc73843181a387b40100228bec6f40cea0e947
Hollow Man <hollowman@hollowman.ml>
2020-11-05 00:12:27 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- js/canvas-ball.js
+++ js/canvas-ball.js
@@ -10,34 +10,19 @@ class Circle {
         this.r = Math.random() * 10;
         this._mx = Math.random();
         this._my = Math.random();
+        this._color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 
     }
 
     //canvas 画圆和画直线
     //画圆就是正常的用canvas画一个圆
-    //画直线是两个圆连线，为了避免直线过多，给圆圈距离设置了一个值，距离很远的圆圈，就不做连线处理
     drawCircle(ctx) {
         ctx.beginPath();
         //arc() 方法使用一个中心点和半径，为一个画布的当前子路径添加一条弧。
         ctx.arc(this.x, this.y, this.r, 0, 360)
         ctx.closePath();
-        ctx.fillStyle = 'rgba(160, 160, 160, 0.2)';
+        ctx.fillStyle = this._color + '44';
         ctx.fill();
-    }
-
-    drawLine(ctx, _circle) {
-        let dx = this.x - _circle.x;
-        let dy = this.y - _circle.y;
-        let d = Math.sqrt(dx * dx + dy * dy)
-        if (d < 150) {
-            ctx.beginPath();
-            //开始一条路径，移动到位置 this.x,this.y。创建到达位置 _circle.x,_circle.y 的一条线：
-            ctx.moveTo(this.x, this.y);   //起始点
-            ctx.lineTo(_circle.x, _circle.y);   //终点
-            ctx.closePath();
-            ctx.strokeStyle = 'rgba(160, 160, 160, 0.2)';
-            ctx.stroke();
-        }
     }
 
     // 圆圈移动
@@ -63,7 +48,7 @@ class currentCirle extends Circle {
         ctx.arc(this.x, this.y, this.r, 0, 360);
         ctx.closePath();
         //ctx.fillStyle = 'rgba(0,0,0,' + (parseInt(Math.random() * 100) / 100) + ')'
-        ctx.fillStyle = 'rgba(255, 77, 54, 0.6)'
+        ctx.fillStyle = 'rgba(255, 77, 54, 0.9)'
         ctx.fill();
 
     }
@@ -77,7 +62,7 @@ class currentCirle extends Circle {
     //设置canvas的高宽
     function set_canvas_size() {
         w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-            h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     }
 
     //更新页面用requestAnimationFrame替代setTimeout
@@ -88,15 +73,9 @@ class currentCirle extends Circle {
         for (let i = 0; i < circles.length; i++) {
             circles[i].move(w, h);
             circles[i].drawCircle(ctx);
-            for (var j = i + 1; j < circles.length; j++) {
-                circles[i].drawLine(ctx, circles[j])
-            }
         }
         if (current_circle.x) {
             current_circle.drawCircle(ctx);
-            for (var k = 1; k < circles.length; k++) {
-                current_circle.drawLine(ctx, circles[k])
-            }
         }
         requestAnimationFrame(draw)
     }
@@ -126,8 +105,7 @@ class currentCirle extends Circle {
         current_circle.x = null;
         current_circle.y = null;
     };
-    //0.1秒后绘制
-    var num = 60;
+    var num = w * h / 9000;
     for (var i = 0; i < num; i++) {
         circles.push(new Circle(Math.random() * w, Math.random() * h));
     }
