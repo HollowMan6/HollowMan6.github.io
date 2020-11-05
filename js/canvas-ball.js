247449b6db58563bff3466a353ec7defda75dede
Hollow Man <hollowman@hollowman.ml>
2020-11-06 00:42:42 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- js/canvas-ball.js
+++ js/canvas-ball.js
@@ -8,8 +8,8 @@ class Circle {
         this.x = x;
         this.y = y;
         this.r = Math.random() * 10;
-        this._mx = Math.random();
-        this._my = Math.random();
+        this._mx = (Math.random() > 0.5) ? Math.random() : -Math.random();
+        this._my = (Math.random() > 0.5) ? Math.random() : -Math.random();
         this._color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
 
     }
@@ -37,7 +37,7 @@ class Circle {
 //鼠标点画圆闪烁变动
 class currentCirle extends Circle {
     constructor(x, y) {
-        super(x, y)
+        super(x, y);
     }
 
     drawCircle(ctx) {
@@ -47,8 +47,8 @@ class currentCirle extends Circle {
         this.r = 8;
         ctx.arc(this.x, this.y, this.r, 0, 360);
         ctx.closePath();
-        //ctx.fillStyle = 'rgba(0,0,0,' + (parseInt(Math.random() * 100) / 100) + ')'
-        ctx.fillStyle = 'rgba(255, 77, 54, 0.9)'
+        //ctx.fillStyle = 'rgba(0,0,0,' + (parseInt(Math.random() * 100) / 100) + ')';
+        ctx.fillStyle = 'rgba(255, 77, 54, 0.9)';
         ctx.fill();
 
     }
@@ -63,6 +63,11 @@ class currentCirle extends Circle {
     function set_canvas_size() {
         w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
         h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        circles = [];
+        num = w * h / 9000;
+        for (var i = 0; i < num; i++) {
+            circles.push(new Circle(Math.random() * w, Math.random() * h));
+        }
     }
 
     //更新页面用requestAnimationFrame替代setTimeout
@@ -77,14 +82,14 @@ class currentCirle extends Circle {
         if (current_circle.x) {
             current_circle.drawCircle(ctx);
         }
-        requestAnimationFrame(draw)
-    }
+        requestAnimationFrame(draw);
+    };
 
     //创建画布，并添加到body中
     var the_canvas = document.createElement("canvas"); //画布
     let ctx = the_canvas.getContext('2d');
-    let w = the_canvas.width = the_canvas.offsetWidth;
-    let h = the_canvas.height = the_canvas.offsetHeight;
+    let w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    let h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     let circles = [];
     let current_circle = new currentCirle(0, 0);
     the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:-1";
