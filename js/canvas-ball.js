7794904a30694c304bdb964c3c09f0062bbad322
Hollow Man <hollowman@hollowman.ml>
2020-11-06 15:34:05 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- js/canvas-ball.js
+++ js/canvas-ball.js
@@ -34,25 +34,6 @@ class Circle {
         this.y += this._my / 2;
     }
 }
-//鼠标点画圆闪烁变动
-class currentCirle extends Circle {
-    constructor(x, y) {
-        super(x, y);
-    }
-
-    drawCircle(ctx) {
-        ctx.beginPath();
-        //注释内容为鼠标焦点的地方圆圈半径变化
-        //this.r = (this.r < 14 && this.r > 1) ? this.r + (Math.random() * 2 - 1) : 2;
-        this.r = 8;
-        ctx.arc(this.x, this.y, this.r, 0, 360);
-        ctx.closePath();
-        //ctx.fillStyle = 'rgba(0,0,0,' + (parseInt(Math.random() * 100) / 100) + ')';
-        ctx.fillStyle = 'rgba(255, 77, 54, 0.9)';
-        ctx.fill();
-
-    }
-}
 
 ! function () {
     //封装方法，压缩之后减少文件大小
@@ -64,7 +45,7 @@ class currentCirle extends Circle {
         w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
         h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
         circles = [];
-        num = w * h / 9000;
+        num = w * h / 20000;
         for (var i = 0; i < num; i++) {
             circles.push(new Circle(Math.random() * w, Math.random() * h));
         }
@@ -79,9 +60,6 @@ class currentCirle extends Circle {
             circles[i].move(w, h);
             circles[i].drawCircle(ctx);
         }
-        if (current_circle.x) {
-            current_circle.drawCircle(ctx);
-        }
         requestAnimationFrame(draw);
     };
 
@@ -91,26 +69,13 @@ class currentCirle extends Circle {
     let w = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     let h = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     let circles = [];
-    let current_circle = new currentCirle(0, 0);
     the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:-1";
     get_by_tagname("body")[0].appendChild(the_canvas);
 
     //初始化画布大小
     set_canvas_size();
-    window.onresize = set_canvas_size;
-    //当时鼠标位置存储，离开的时候，释放当前位置信息
-    window.onmousemove = function (e) {
-        e = e || window.event;
-        current_circle.x = e.clientX;
-        current_circle.y = e.clientY;
-    }, window.onmouseout = function () {
-        current_circle.x = null;
-        current_circle.y = null;
-    }, window.ontouchend = function () {
-        current_circle.x = null;
-        current_circle.y = null;
-    };
-    var num = w * h / 9000;
+    window.addEventListener("resize", set_canvas_size);
+    var num = w * h / 20000;
     for (var i = 0; i < num; i++) {
         circles.push(new Circle(Math.random() * w, Math.random() * h));
     }
