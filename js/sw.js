055627d987f771a643a9a2537d17261eb12c3598
Hollow Man <hollowman@hollowman.ml>
2021-02-21 21:12:00 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

---
+++ js/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function (clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
+});
