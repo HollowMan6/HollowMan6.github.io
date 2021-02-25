ab3f64d16ddc56a35c449ebae266ff7b45477df7
Hollow Man <hollowman@hollowman.ml>
2021-02-26 00:10:06 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- js/sw.js
+++ js/sw.js
@@ -1,37 +1,25 @@
 self.addEventListener('push', function (event) {
-  const title = event.data.text().slice(0, 2) + ': 兰州大学疫情期间自动定时健康打卡工作流';
-  const options = {
-    requireInteraction: true,
-    vibrate: [200, 100, 200],
-    icon: "https://avatars.githubusercontent.com/oa/1472343?s=64&v=4",
-    data: event.data.text().slice(2),
-    body: "点击通知查看打卡记录工作流!"
-  };
-
+  var data = JSON.parse(event.data.text());
+  var title = data["title"];
+  delete data.title;
+  var options = data;
   event.waitUntil(self.registration.showNotification(title, options));
 });
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  if (event.notification.data) {
-    event.waitUntil(clients.matchAll({
-      type: "window"
-    }).then(function () {
-      if (clients.openWindow)
-        return clients.openWindow(event.notification.data);
-    }));
-  } else {
-    event.waitUntil(clients.matchAll({
-      includeUncontrolled: true,
-      type: "window"
-    }).then(function (clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        var client = clientList[i];
-        if ('focus' in client)
-          return client.focus();
-      }
-      if (clients.openWindow)
-        return clients.openWindow('/');
-    }));
-  }
+  event.waitUntil(clients.matchAll({
+    includeUncontrolled: true,
+    type: "window"
+  }).then(function (clientList) {
+    if (clients.openWindow && event.notification.data)
+      return clients.openWindow(event.notification.data);
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if ('focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
 });
