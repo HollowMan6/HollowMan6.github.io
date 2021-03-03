2d1ff0c3135f5dcb1c78e10e29664be9f0f22446
Hollow Man <hollowman@hollowman.ml>
2021-03-03 23:14:52 +0800

    Latest push
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- sw.js
+++ sw.js
@@ -40,9 +40,18 @@ self.addEventListener("fetch", function (event) {
           return response;
         });
       }).catch(function () {
-        if (r && event.request.url.indexOf('.html') != -1) {
-          event.waitUntil(self.registration.showNotification("You are in offline mode! \n正在脱机模式下使用!"))
-        }
+        var tempList=[];
+        event.waitUntil(clients.matchAll({
+          type: "window"
+        }).then(function (clientList) {
+          for (var i = 0; i < clientList.length; i++) {
+            tempList.push(clientList[i].url);
+          }
+        }).then(function(){
+          if (r && tempList.indexOf(event.request.url) != -1) {
+            event.waitUntil(self.registration.showNotification("You are in offline mode! \n正在脱机模式下使用!"));
+          }
+        }));
         return r;
       });
     })
